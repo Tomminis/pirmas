59cc45752b12b1ab69ae27fd30d98b0d904fa82b
Ruta <tbuinevicius@gmail.com>
2017-03-03 20:38:36 +0200

    trecia uzduotis

---
+++ src/trecias.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Created by Ruta on 2017-03-03.
+ */
+public class trecias {
+    public static void main(String[] args) {
+        System.out.print("iveskite du skaicius");
+        Scanner sk = new Scanner(System.in);
+        int skaicius = sk.nextInt();
+        int skaicius1 = sk.nextInt();
+        int suma = skaicius + skaicius1;
+        System.out.print(suma);
+        System.out.print("iveskite du skaicius");
+        Scanner sk1 = new Scanner(System.in);
+        int skaicius2 = sk1.nextInt();
+        int skaicius3= sk1.nextInt();
+
+
+
+
+        int skirtumas = skaicius - skaicius1;
+        System.out.print(skirtumas);
+    }
+
+
+
+
+
+}
