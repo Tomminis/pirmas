53137808cf0fe1435fc737dcc141832cc5e6f023
Ruta <tbuinevicius@gmail.com>
2017-03-03 20:18:16 +0200

    antra uzduotis

--- src/Antra.java
+++ src/Antra.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 /**
  * Created by Ruta on 2017-03-03.
  */
 public class Antra {
-    public static void main()
+    public static void main(String[] args){
+        System.out.print("iveskite zodi");
+        Scanner z = new Scanner(System.in) ;
+        String zodis = z.nextLine();
+        System.out.print(zodis);
+        System.out.print(zodis);
+        System.out.print(zodis);
+        System.out.print(zodis);
+        System.out.print(zodis);
+    }
 }
