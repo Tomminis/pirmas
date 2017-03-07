f32ecce4625085d19e085d86b22cd7e626b2e94c
Ruta <tbuinevicius@gmail.com>
2017-03-07 20:19:18 +0200

    ketvirta

--- src/com/uzduotis10/Uzduotis10.java
+++ src/com/uzduotis10/Uzduotis10.java
@@ -7,24 +7,25 @@ import java.util.Scanner;
  */
 public class Uzduotis10 {
     public Uzduotis10() {
-        System.out.println("iveskite 5 skaicius");
+        System.out.println("iveskite 3 zodzius");
         Scanner s = new Scanner(System.in);
-        int[] mas = new int[5];
+        String[] mas = new String[3];
 
 
-        int suma = 0;
-        for (int i = 0; i < mas.length; i++) {
-            System.out.print("iveskite " + (i + 1) + "skaiciu kuris turi buti sveikasis");
-            mas[i] = s.nextInt();
-        }
         //int suma = 0;
         for (int i = 0; i < mas.length; i++) {
-            System.out.println("ivestas skaicius " + mas[i]);
-            suma += mas[i];
+            System.out.print("iveskite " + (i + 1) );
+            mas[i] = s.next();
+        }
+        String suma ="";
+        for (int i = 0; i < mas.length; i++) {
+            //System.out.println("ivestas skaicius " + mas[i]);
+            suma +=" "+ mas[i];
 
         }
-        System.out.print("skaiciu suma yra: " + suma);
+        System.out.print("jus ivedete: " + suma );
 
     }
+    public void nuskaitymas
 
 }
